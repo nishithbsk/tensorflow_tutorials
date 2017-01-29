efb603b217f3e7b91c66f608aa5ceb574490ff9a
Nishith Khandwala <nishith@stanford.edu>
2017-01-29 02:49:59 -0800

    Added max_pool and assertions

--- cs20si/autoencoder.py
+++ cs20si/autoencoder.py
@@ -1,18 +1,18 @@
 import numpy as np
 import tensorflow as tf
 
-def conv(input, name, weight_dims, stride_dims, padding='SAME'):
-    assert(len(weight_dims) == 4) # height, width, num_channels in and out
+def conv(input, name, filter_dims, stride_dims, padding='SAME'):
+    assert(len(filter_dims) == 4) # height, width, num_channels in and out
     assert(len(stride_dims) == 2) # stride height and width
 
-    filter_h, filter_w, num_channels_in, num_channels_out = weight_dims
+    filter_h, filter_w, num_channels_in, num_channels_out = filter_dims
     stride_h, stride_w = stride_dims
 
     with tf.variable_scope(name) as scope:
-        weights = tf.get_variable('weights', weight_dims)
+        weights = tf.get_variable('weights', filter_dims)
         biases = tf.get_variable('biases', [num_channels_out])
         out = tf.nn.conv2d(input,
-                           weight_dims,
+                           filter_dims,
                            [1, stride_h, stride_w, 1],
                            padding=padding)
         out = tf.nn.bias_add(out, biases)
@@ -22,6 +22,8 @@ def deconv():
     raise NotImplementedError
 
 def fc(input, name, out_dim):
+    assert(type(out_dim) == int)
+
     with tf.variable_scope(name) as scope:
         input_dims = input.get_shape()
         # the input to the fc layer should be flattened
@@ -39,6 +41,17 @@ def fc(input, name, out_dim):
         weights = tf.get_variable('weights', [in_dim, out_dim])
         biases = tf.get_variable('biases', [out_dim])
         return tf.nn.relu_layer(flat_input, weights, biases, name=scope.name)
+
+def max_pool(input, name, filter_dims, stride_dims, padding='SAME'):
+    assert(len(filter_dims) == 2) # filter height and width
+    assert(len(stride_dims) == 2) # stride height and width
+
+    filter_h, filter_w = filter_dims
+    stride_h, stride_w = stride_dims
+    return tf.nn.max_pool(input, 
+                          [1, filter_h, filter_w, 1],
+                          [1, stride_h, stride_w, 1],
+                          padding=padding)
 
 def encoder():
     raise NotImplementedError
