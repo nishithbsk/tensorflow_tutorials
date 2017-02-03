962eeb077112b7804d8a6902c1abf05f3025f965
Nishith Khandwala <nishith@stanford.edu>
2017-02-03 04:57:45 -0800

    Finished conv autoencoder

--- cs20si/autoencoder.py
+++ cs20si/autoencoder.py
@@ -3,38 +3,42 @@ import tensorflow as tf
 from layers import *
 
 def encoder(input):
-    input_reshaped = tf.reshape(input, [None,
-                                        image_dims[0],
-                                        image_dims[1],
-                                        image_dims[2]])
-    conv1 = conv(input_reshaped, 'conv1', [5, 5, 32], [2, 2])
-    conv2 = conv(conv1, 'conv2', [5, 5, 64], [2, 2])
-    conv3 = conv(conv2, 'conv3', [3, 3, 128], [2, 2])
-    conv4 = conv(conv3, 'conv4', [3, 3, 256], [2, 2])
-    conv5 = conv(conv4, 'conv5', [3, 3, 512], [2, 2])
-    fc_enc = fc(conv5, 'fc_enc', hidden_size, non_linear_fn=None)
+    conv1 = conv(input, 'conv1', [3, 3, 1], [2, 2])
+    conv2 = conv(conv1, 'conv2', [3, 3, 8], [2, 2])
+    conv3 = conv(conv2, 'conv3', [3, 3, 8], [2, 2])
+    fc_enc = fc(conv3, 'fc_enc', 100, non_linear_fn=None)
     return fc_enc
 
 def decoder(input):
-    fc_dec = fc(input, 'fc_dec', 7*7*512)
-    fc_dec_reshaped = tf.reshape(fc_dec, [None,
-                                          7, 7, 512])
-    deconv1 = deconv(fc1, 'deconv1', [3, 3, 256], [2, 2])
-    deconv2 = deconv(deconv1, 'deconv2', [3, 3, 128], [2, 2])
-    deconv3 = deconv(deconv2, 'deconv3', [3, 3, 64], [2, 2])
-    deconv4 = deconv(deconv3, 'deconv4', [5, 5, 32], [2, 2])
-    deconv5 = deconv(deconv4, 'deconv5', [5, 5, 3], [2, 2], non_linear_fn=tf.sigmoid)
-    return deconv5
+    fc_dec = fc(input, 'fc_dec', 128)
+    fc_dec_reshaped = tf.reshape(fc_dec, [-1, 4, 4, 8])
+    deconv1 = deconv(fc_dec_reshaped, 'deconv1', [3, 3, 8], [2, 2])
+    deconv2 = deconv(deconv1, 'deconv2', [8, 8, 1], [2, 2], padding='VALID')
+    deconv3 = deconv(deconv2, 'deconv3', [7, 7, 1], [1, 1], padding='VALID',
+                     non_linear_fn=tf.sigmoid)
+    return deconv3
 
-def autoencoder():
+def encoder_fc(input):
+    input_flat = tf.reshape(input, (-1, 784))
+    fc1 = fc(input_flat, 'fc1', 128)
+    fc2 = fc(fc1, 'fc2', 64)
+    fc3 = fc(fc2, 'fc3', 32)
+    return fc3
+
+def decoder_fc(input):
+    fc1_dec = fc(input, 'fc1_dec', 64)
+    fc2_dec = fc(fc1_dec, 'fc2_dec', 128)
+    fc3_dec = fc(fc2_dec, 'fc3_dec', 784, non_linear_fn=tf.sigmoid)
+    fc3_dec_reshaped = tf.reshape(fc3_dec, (-1, 28, 28, 1))
+    return fc3_dec_reshaped
+
+def autoencoder(input_shape):
     input_image = tf.placeholder(tf.float32,
-                                 [None,
-                                  image_dims[0],
-                                  image_dims[1],
-                                  image_dims[2]], name='input_image')
+                                 input_shape,
+                                 name='input_image')
 
     with tf.variable_scope('autoencoder') as scope:
         encoding = encoder(input_image)
         reconstructed_image = decoder(encoding)
-    return input_image, reconstructed_image
+        return input_image, reconstructed_image
 
--- cs20si/train.py
+++ cs20si/train.py
@@ -1,11 +1,14 @@
 import tensorflow as tf
 
-from data_utils import *
+from utils import *
 from autoencoder import *
 
-batch_size = ...
-lr = ...
-num_iters = ...
+batch_size = 100
+batch_shape = (batch_size, 28, 28, 1)
+num_visualize = 10
+
+lr = 0.01
+num_epochs = 50
 
 def calculate_loss(original, reconstructed):
     return tf.div(tf.reduce_sum(tf.square(tf.sub(reconstructed,
@@ -13,7 +16,7 @@ def calculate_loss(original, reconstructed):
                   tf.constant(float(batch_size)))
 
 def train(dataset):
-    input_image, reconstructed_image = autoencoder()
+    input_image, reconstructed_image = autoencoder(batch_shape)
     loss = calculate_loss(input_image, reconstructed_image)
     optimizer = tf.train.GradientDescentOptimizer(lr).minimize(loss)
 
@@ -21,18 +24,23 @@ def train(dataset):
     with tf.Session() as session:
         session.run(init)
 
+        dataset_size = len(dataset.train.images)
+        print "Dataset size:", dataset_size
+        num_iters = (num_epochs * dataset_size)/batch_size
+        print "Num iters:", num_iters
         for step in xrange(num_iters):
-            input_batch, _ = dataset.train.next_batch(batch_size)
+            input_batch  = get_next_batch(dataset.train, batch_size)
             loss_val,  _ = session.run([loss, optimizer], 
                                        feed_dict={input_image: input_batch})
-            print "Loss at step", step, ":", loss_val
+            if step % 1000 == 0:
+                print "Loss at step", step, ":", loss_val
 
+        test_batch = get_next_batch(dataset.test, batch_size)
         reconstruction = session.run(reconstructed_image,
-                                     feed_dict={input_image: dataset.test.images})
-
-
+                                     feed_dict={input_image: test_batch})
+        visualize(test_batch, reconstruction, num_visualize)
 
 if __name__ == '__main__':
-    dataset = load_dataset('mnist')
+    dataset = load_dataset()
     train(dataset)
     
